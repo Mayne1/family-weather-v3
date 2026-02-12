526ee281fff3f21c437c3d193154541be1fdfc7f
Tremayne Clinton <maynex1x@gmail.com>
2026-02-12 00:36:33 -0800

    Use tracked public Firebase config for production

---
+++ srcfirebaseconfig.public.js
@@ -0,0 +1,6 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDCZpxyyGJeoIcutk8o_h-96Syo3h8gsv8",
+  authDomain: "the-family-weather.firebaseapp.com",
+  projectId: "the-family-weather",
+  appId: "1:316642786319:web:545d5598d31da95a3d377a"
+};
